015b2f55026cb17a613ed91177a95e6818ed9e5d
megagas <megagas@gmail.com>
2026-05-11 14:42:47 +0700

    revert: remove pkce flow

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,10 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://jthqwchnplaocgiqazov.supabase.co',
-  'sb_publishable_wKgCT6WFd94uevg1R43_EQ_-TTLUzOJ',
-  {
-    auth: {
-      flowType: 'pkce',
-    }
-  }
+  'sb_publishable_wKgCT6WFd94uevg1R43_EQ_-TTLUzOJ'
 )
