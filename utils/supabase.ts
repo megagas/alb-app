828cc9373080834a2f40ca1aadba4357771da2bb
megagas <megagas@gmail.com>
2026-05-11 14:40:01 +0700

    fix: wallet change warning popup

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,5 +2,10 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://jthqwchnplaocgiqazov.supabase.co',
-  'sb_publishable_wKgCT6WFd94uevg1R43_EQ_-TTLUzOJ'
+  'sb_publishable_wKgCT6WFd94uevg1R43_EQ_-TTLUzOJ',
+  {
+    auth: {
+      flowType: 'pkce',
+    }
+  }
 )
